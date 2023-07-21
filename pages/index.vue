8ed2ecc850423398001923efbd0d0ccfb6459ec6
Farihul Rouf <farihulrouf@gmail.com>
2023-07-21 15:55:52 +0700

    detail

--- pages/index.vue
+++ pages/index.vue
@@ -84,8 +84,7 @@
       <div class="flex justify-between items-center">
         <div class="w-auto">
           <h2 class="text-3xl text-gray-900 mb-8">
-            New projects you can <br />
-            taken care of
+            Bring your website to life
           </h2>
         </div>
         <div class="w-auto mt-5">
@@ -98,7 +97,7 @@
         <div
           v-for="product in products.data"
           :key="product.id"
-          class="w-full p-5 rounded-20"
+          class="w-full rounded-20"
         >
         <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
                 <h3 class="mb-4 text-2xl font-semibold"> {{ product.name }}</h3>
@@ -135,7 +134,17 @@
                         <span>Free updates: <span class="font-semibold">6 months</span></span>
                     </li>
                 </ul>
-                <a href="#" class="text-white bg-indigo-700 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
+            
+                <button
+                @click="
+                $router.push({
+                  name: 'products-id',
+                  params: { id: product.id },
+                })
+              "
+                    class="text-white bg-indigo-700 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">
+                    Get startedx
+                </button>
             </div>
           </div>
         <!--
