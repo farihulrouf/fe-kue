5166e463392976757d291a654db138d66967be5e
Farihul Rouf <farihulrouf@gmail.com>
2023-08-12 19:46:43 +0700

    init 2

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,197 +1,204 @@
 <template>
-    <div class="project-page">
-      <section class="dashboard-header pt-5">
-        <div class="container mx-auto relative">
-          <Navbar />
+  <div class="project-page">
+    <section class="dashboard-header pt-5">
+      <div class="container mx-auto relative">
+        <Navbar />
+      </div>
+    </section>
+    <section class="container mx-auto pt-8 mx-6">
+      <div class="flex justify-between items-center mb-6">
+        <div class="w-3/4 mr-6 px-6">
+          <h2 class="text-4xl text-gray-900 mb-2 font-medium">Dashboard</h2>
+          <ul class="flex mt-2">
+            <li class="mr-6">
+              <nuxt-link
+                class="text-gray-500 hover:text-gray-800"
+                to="/dashboard"
+              >
+                My Transactions
+              </nuxt-link>
+            </li>
+            <!--
+            <li class="mr-6">
+              <a class="text-gray-800 font-bold" href="#">
+                Your Transactions
+              </a>
+            </li>
+            -->
+          </ul>
         </div>
-      </section>
-      <section class="container mx-auto pt-8">
-        <div class="flex justify-between items-center mb-6">
-          <div class="w-3/4 mr-6">
-            <h2 class="text-4xl text-gray-900 mb-2 font-medium">Dashboard</h2>
-            <ul class="flex mt-2">
-              <li class="mr-6">
-                <a class="text-gray-800 font-bold" href="#"> Your Message </a>
-              </li>
-              <li class="mr-6">
-                <nuxt-link
-                  class="text-gray-500 hover:text-gray-800"
-                  to="/dashboard/transactions"
-                >
-                  Your Transactions
-                </nuxt-link>
-              </li>
-            </ul>
-          </div>
-          <div class="w-1/4 text-right">
-            <nuxt-link
-              to="/dashboard/projects/create"
-              class="bg-orange-button hover:bg-green-button text-white font-bold py-4 px-4 rounded inline-flex items-center"
-            >
-              + Create product
-            </nuxt-link>
-          </div>
-        </div>
-        <hr />
-        <div class="block mb-2">
+      </div>
+      <hr />
+      <div class="block mb-2">
+        <div
+          class="w-full lg:max-w-full lg:flex mb-4"
+          v-for="transaction in transactions.data"
+          :key="transaction.id"
+        >
+        <!--
           <div
-            class="w-full lg:max-w-full lg:flex mb-4"
-            v-for="product in products.data"
-            :key="product.id"
+            class="border h-48 lg:h-auto lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+            :style="
+              'background-color: #bbb; background-position: center; background-image: url(\'' +
+              $axios.defaults.baseURL +
+              '/' +
+              transaction.product.image_url +
+              '\')'
+            "
+          ></div>
+        -->
+          <div
+            class="w-full px-12 mt-8 border-b flex justify-between"
           >
-            <div
-              class="border h-48 lg:h-auto lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-              :style="
-                'background-color: #bbb; background-position: center; background-image: url(\'' +
-                $axios.defaults.baseURL +
-                '/' +
-                product.image_url +
-                '\')'
-              "
-            ></div>
-            <nuxt-link
-              :to="'/dashboard/projects/' + product.id"
-              class="w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-8 flex flex-col justify-between leading-normal"
-            >
-              <div class="mb-8">
-                <div class="text-gray-900 font-bold text-xl mb-1">
-                  {{ product.name }}
+            <div>
+              <div class="text-gray-900 font-bold text-xl mb-1">
+                {{ transaction.product.name }}
+              </div>
+              <p class="text-sm text-gray-600 flex items-center mb-2">
+                Rp.
+                {{ new Intl.NumberFormat().format(transaction.amount) }}
+                &middot; {{ transaction.created_at }} &middot;
+              </p>
+              <div
+                  v-if="transaction.status === 'pending'"
+                  class="tml-3 my-5 bg-yellow-600 p-1 w-20"
+                >
+                  <div
+                    class="uppercase text-xs leading-4 font-semibold text-center text-yellow-100"
+                  >
+                    {{ transaction.status }}
+                  </div>
                 </div>
-                <p class="text-sm text-gray-600 flex items-center mb-2">
-                  Rp.
-                  {{ new Intl.NumberFormat().format(product.goal_amount) }}
-                  &middot;
-                  {{ (product.current_amount / product.goal_amount) * 100 }}%
-                </p>
-                <p class="text-gray-700 text-base">
-                  {{ product.short_description }}
-                </p>
-              </div>
-              <div class="flex items-center">
-                <button class="bg-green-button text-white py-2 px-4 rounded">
-                  Detail
-                </button>
-              </div>
-            </nuxt-link>
+                <div v-else class="tml-3 my-5 bg-teal-600 p-1 w-20">
+                  <div
+                    class="uppercase text-xs leading-4 font-semibold text-center text-yellow-100"
+                  >
+                    {{ transaction.status }}
+                  </div>
+                </div>
+            </div>
+            <div>
+              <button class="bg-teal-500 px-4 rounded-md text-gray-600">
+                Detail
+              </button>
+            </div>
           </div>
         </div>
-      </section>
-      <div class="cta-clip -mt-20"></div>
-      <section class="call-to-action bg-purple-progress pt-64 pb-10"></section>
-      <Footer />
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    middleware: 'auth',
-    async asyncData({ $axios, app }) {
-      const products = await $axios.$get(
-        '/api/v1/products?user_id=' + app.$auth.user.id
-      )
-      return { products }
-    },
+      </div>
+    </section>
+    <div class="cta-clip -mt-20"></div>
+    <section class="call-to-action bg-purple-progress pt-64 pb-10"></section>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  async asyncData({ $axios }) {
+    const transactions = await $axios.$get('/api/v1/transactions')
+    return { transactions }
+  },
+}
+</script>
+
+<style lang="scss">
+.dashboard-header {
+  background-image: url('/auth-background.svg');
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-color: #3b41e3;
+  background-size: 250px;
+  height: 100px;
+}
+
+.cta-clip {
+  position: relative;
+  top: 200px;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 300px;
+  background-position: top right;
+  background-size: 300px;
+  background-repeat: no-repeat;
+  background-color: #fff;
+  transform: skewY(4deg);
+  transform-origin: bottom right;
+}
+
+.call-to-action {
+  background-image: url('/auth-background.svg');
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-size: 450px;
+}
+
+.card-project {
+  transition: all 0.3s ease 0s, opacity 0.5s cubic-bezier(0.5, 0, 0, 1) 1ms;
+  max-height: 485px;
+  overflow: hidden;
+
+  .button-cta {
+    opacity: 0;
+    transition: all 300ms ease;
   }
-  </script>
-  
-  <style lang="scss">
-  .dashboard-header {
-    background-image: url('/auth-background.svg');
-    background-position: top right;
-    background-repeat: no-repeat;
-    background-color: #3b41e3;
-    background-size: 250px;
-    height: 100px;
-  }
-  
-  .cta-clip {
-    position: relative;
-    top: 200px;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    width: 100%;
-    height: 300px;
-    background-position: top right;
-    background-size: 300px;
-    background-repeat: no-repeat;
-    background-color: #fff;
-    transform: skewY(4deg);
-    transform-origin: bottom right;
-  }
-  
-  .call-to-action {
-    background-image: url('/auth-background.svg');
-    background-position: top right;
-    background-repeat: no-repeat;
-    background-size: 450px;
-  }
-  
-  .card-project {
-    transition: all 0.3s ease 0s, opacity 0.5s cubic-bezier(0.5, 0, 0, 1) 1ms;
-    max-height: 485px;
-    overflow: hidden;
-  
+
+  &:hover {
+    box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.15);
+
     .button-cta {
-      opacity: 0;
+      opacity: 1;
       transition: all 300ms ease;
     }
-  
-    &:hover {
-      box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.15);
-  
-      .button-cta {
-        opacity: 1;
-        transition: all 300ms ease;
-      }
-  
-      .progress-bar,
-      .progress-info {
-        opacity: 0;
-        height: 0px;
-        margin: 0px;
-        padding: 0px;
-        transition: all 300ms ease;
-      }
+
+    .progress-bar,
+    .progress-info {
+      opacity: 0;
+      height: 0px;
+      margin: 0px;
+      padding: 0px;
+      transition: all 300ms ease;
     }
   }
-  
-  footer {
-    z-index: inherit;
+}
+
+footer {
+  z-index: inherit;
+}
+
+.hero-underline {
+  text-decoration-color: #1abc9c;
+}
+
+.testimonial-user {
+  opacity: 0.4;
+  &.active {
+    opacity: 1;
+    border: 5px solid #fff;
+    box-shadow: 0 0 0 1px #3b41e3;
   }
-  
-  .hero-underline {
-    text-decoration-color: #1abc9c;
+}
+
+.list-check {
+  li {
+    background: url('/icon-checklist.svg') no-repeat left 8px;
+    padding: 6px 0px 3px 28px;
   }
-  
-  .testimonial-user {
-    opacity: 0.4;
-    &.active {
-      opacity: 1;
-      border: 5px solid #fff;
-      box-shadow: 0 0 0 1px #3b41e3;
-    }
+}
+
+.item-thumbnail:hover {
+  background-color: #ff872e;
+  border-radius: 20px;
+  &:after {
+    position: absolute;
+    top: 38%;
+    left: 41%;
+    content: url('/icon-thumbnail-hover.svg');
   }
-  
-  .list-check {
-    li {
-      background: url('/icon-checklist.svg') no-repeat left 8px;
-      padding: 6px 0px 3px 28px;
-    }
+  img {
+    opacity: 0.3;
   }
-  
-  .item-thumbnail:hover {
-    background-color: #ff872e;
-    border-radius: 20px;
-    &:after {
-      position: absolute;
-      top: 38%;
-      left: 41%;
-      content: url('/icon-thumbnail-hover.svg');
-    }
-    img {
-      opacity: 0.3;
-    }
-  }
-  </style>
-  
+}
+</style>
